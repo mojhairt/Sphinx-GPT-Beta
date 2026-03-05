60b55deeaca62419d83aa066fadb37bf875f0d0b
HAMO <Mohamedkh476@gmail.com>
2026-03-06 01:26:49 +0200

    Add CORS to app.py to fix connection error

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+# السماح للواجهة بالاتصال بالخادم
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Question(BaseModel):
     text: str
